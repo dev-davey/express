59cd0da1bfef12197e42874662320cf498d78a61
davey <davey89451@gmail.com>
2021-08-10 18:21:59 -0700

    routes added

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+const mainRoutes = require('./routes')
+const cardRoutes = require('./routes/cards')
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('index')
-});
 
-app.get('/card', (req, res) => { 
-    res.render('card', {prompt:"who is burried in grants tomb" })
-});
 
-app.get('/hello', (req, res) => { 
-    res.render('hello')
-});
-
-app.post('/hello', (req, res) => {
-    res.render('hello', {username: req.body.username})
-    console.dir(req.body)
+app.use((req, res, next) => {
+    console.log('two');
+    next()
 })
 
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+app.use((req, res, next) => {
+    const err = new Error('not found')
+    err.status = 404;
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error', err);
+})
 
 app.listen(3000, () => {
-    console.log('app is now listening on port 3000')
+    console.log('The application is running on localhost:3000!')
 });
